9d6032288aa9b1a70d77c1fbdd14f7cea274d42f
Alessio Burgio <alessioburgio85@gmail.com>
2020-09-23 19:50:43 +0200

    default state

--- app3.js
+++ app3.js
@@ -2,7 +2,7 @@
 const form = document.getElementById('form');
 
 // KEY 
-const API_KEY = 'AIzaSyB_e20EKxEoQdw2WBx69UJHdH1aGytZGPc';
+const API_KEY = 'AIzaSyAxYifWHqq1v7fnjp-SK3tNySlkKHgk4Dc';
 
 // EVENT LISTENER on SUBMIT
 form.addEventListener('submit', (e) => {
@@ -31,10 +31,11 @@ const displaySearchResults = (arrayOfResults) => {
     console.log(arrayOfResults);
     
     const imagesPaginated = document.getElementById('imagesPaginated');
+    imagesPaginated.innerHTML = '';
 
     const renderedImages = arrayOfResults
         .map((resultToDisplay) => {
-            console.log(resultToDisplay);
+            
             
             if(resultToDisplay.pagemap.thumbnail) {
             return `
@@ -59,75 +60,72 @@ const displaySearchResults = (arrayOfResults) => {
 
 
    
-        const list_items = arrayOfResults;
-        const webResultsPaginated = document.getElementById('webResultsPaginated');
-        const pageNumbers = document.getElementById('buttonsWrapper');
+    const list_items = arrayOfResults;
+    const webResultsPaginated = document.getElementById('webResultsPaginated');
+    const pageNumbers = document.getElementById('buttonsWrapper');
 
-        let current_page = 1;
-        let rows = 5;
+    webResultsPaginated.innerHTML = '';
+
+    let current_page = 1;
+    let rows = 5;
 
 
-        function displayList (items, wrapper, rows_per_page, page) {
-            wrapper.innerHTML = "";
-            page--;
+    function displayList (items, wrapper, rows_per_page, page) {
+        wrapper.innerHTML = "";
+        page--;
+    
+        let start = rows_per_page * page;
+        let end = start + rows_per_page;
+        let paginatedItems = items.slice(start, end);
         
-            let start = rows_per_page * page;
-            let end = start + rows_per_page;
-            let paginatedItems = items.slice(start, end);
-           
-            let item_element = paginatedItems
-            .map((item) => {
+        let item_element = paginatedItems
+        .map((item) => {
 
-                return `
-                <li class="character">
-                    <h2>${item.title}</h2>
-                    <a href="${item.link}" >${item.formattedUrl}</a>
-                    <p>${item.htmlSnippet}</p>
-                </li>
-            `;
-            }).join('');
+            return `
+            <li class="character">
+                <h2>${item.title}</h2>
+                <a href="${item.link}" >${item.formattedUrl}</a>
+                <p>${item.htmlSnippet}</p>
+            </li>
+        `;
+        }).join('');
 
-            webResultsPaginated.innerHTML = item_element;
+        webResultsPaginated.innerHTML = item_element;
+    }
+
+    
+    function setupPagination (items, wrapper, rows_per_page) {
+        wrapper.innerHTML = "";
+    
+        let page_count = Math.ceil(items.length / rows_per_page);
+        for (let i = 1; i < page_count + 1; i++) {
+            let btn = paginationButton(i, items);
+            wrapper.appendChild(btn);
         }
+    }
+    
+    function paginationButton (page, items) {
+        let button = document.createElement('button');
+        button.innerText = page;
+    
+        if (current_page == page) button.classList.add('active');
+    
+        button.addEventListener('click', function () {
+            current_page = page;
+            displayList(items, webResultsPaginated, rows, current_page);
+    
+            let current_btn = document.querySelector('.pageNumbers button.active');
+            console.log(current_btn);
+            
+            current_btn.classList.remove('active');
+    
+            button.classList.add('active');
+        });
+    
+        return button;
+    }
+        
+    displayList(list_items, webResultsPaginated, rows, current_page);
+    setupPagination(list_items, pageNumbers, rows);
 
-        
-        function setupPagination (items, wrapper, rows_per_page) {
-            wrapper.innerHTML = "";
-        
-            let page_count = Math.ceil(items.length / rows_per_page);
-            for (let i = 1; i < page_count + 1; i++) {
-                let btn = paginationButton(i, items);
-                wrapper.appendChild(btn);
-            }
-        }
-        
-        function paginationButton (page, items) {
-            let button = document.createElement('button');
-            button.innerText = page;
-        
-            if (current_page == page) button.classList.add('active');
-        
-            button.addEventListener('click', function () {
-                current_page = page;
-                displayList(items, webResultsPaginated, rows, current_page);
-        
-                let current_btn = document.querySelector('.pageNumbers button.active');
-                console.log(current_btn);
-                
-                current_btn.classList.remove('active');
-        
-                button.classList.add('active');
-            });
-        
-            return button;
-        }
-        
-        displayList(list_items, webResultsPaginated, rows, current_page);
-        setupPagination(list_items, pageNumbers, rows);
-
-
-
-
-
-        
 };
